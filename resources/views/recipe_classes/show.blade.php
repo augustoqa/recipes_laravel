8d3d8ddc5e9edf20a7eacf76aa2712181051bbbf
Cesar Acual <chechaacual@gmail.com>
2019-12-09 10:22:29 -0600

    Add bootstrap CSS

--- resources/views/recipe_classes/show.blade.php
+++ resources/views/recipe_classes/show.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Detail Recipe Class</title>
-</head>
-<body>
-    <h1>Detail Recipe Class #<?= e($id) ?></h1>
+@extends('layouts.app')
+
+@section('title', 'Detail Recipe')
+
+@section('content')
+    <h1>Detail Recipe Class #{{ $id }}</h1>
     <p>Main course</p>
-</body>
-</html>
+@endsection
